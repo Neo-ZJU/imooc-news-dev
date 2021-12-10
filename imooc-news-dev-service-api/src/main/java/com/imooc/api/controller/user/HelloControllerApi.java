765a677fb141de82cd17ddc99625d4131264c0cb
niupengcheng <npc20@mails.tsinghua.edu.cn>
2021-12-10 08:28:30 +0800

    mybatisi

--- imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
+++ imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
@@ -3,7 +3,7 @@ package com.imooc.api.controller.user;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public interface HelloController {
+public interface HelloControllerApi {
     @GetMapping("/hello")
     public Object hello();
 }
--- imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
+++ imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/HelloController.java
@@ -1,12 +1,24 @@
 package com.imooc.user.controller;
 
+import com.imooc.api.controller.user.HelloControllerApi;
+import com.imooc.grace.result.IMOOCJSONResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/hello")
+public class HelloController implements HelloControllerApi {
+
+
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     public Object hello(){
-        return "hello";
+        //System.out.println("test");
+        logger.info("debug hello");
+        logger.error("testError");
+        logger.warn("testWarn");
+        return IMOOCJSONResult.errorMsg("未知错误");
     }
+
 }
